35379a3cd8cdb126844c9d4fdd22889ff84b37b5
Mohammad Samiul Islam <samiul@google.com>
2021-09-10 13:24:25 +0100

    Wait for files to be cleaned up by apexd
    Previously, apexd could be waiting for boot status up to 30 seconds
    after boot was completed before running clean up operations.
    
    In this CL, we have reduced that time to 10 seconds and updated the test
    so that they take into consideration that there could be a delay between
    boot complete and clean up operations being ran by apexd.
    
    Bug: 199256259
    Test: atest ApexCompressionTests
    Change-Id: I953f06b552bf2e89a92e95d8a83483b5110c155d

--- apexd/apexd_lifecycle.cpp
+++ apexd/apexd_lifecycle.cpp
@@ -43,12 +43,12 @@ void ApexdLifecycle::WaitForBootStatus(
   while (!boot_completed_) {
     // Check for change in either crashing property or sys.boot_completed
     // Wait for updatable_crashing property change for most of the time
-    // (arbitrary 30s), briefly check if boot has completed successfully,
+    // (arbitrary 10s), briefly check if boot has completed successfully,
     // if not continue waiting for updatable_crashing.
     // We use this strategy so that we can quickly detect if an updatable
     // process is crashing.
     if (WaitForProperty("sys.init.updatable_crashing", "1",
-                        std::chrono::seconds(30))) {
+                        std::chrono::seconds(10))) {
       auto name = GetProperty("sys.init.updatable_crashing_process_name", "");
       LOG(ERROR) << "Native process '" << (name.empty() ? "[unknown]" : name)
                  << "' is crashing. Attempting a revert";
