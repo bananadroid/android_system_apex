1b8f2f7de9c6a1a814864bf0b82811f7f990d58c
Nikita Ioffe <ioffe@google.com>
2021-09-14 23:51:36 +0100

    Add uid checks to apexservice binder calls
    Since apexservice binder is now exposed to more than just system_server,
    we should add uid based checks for method exposed by apexservice. This
    is mostly to make sure that stateful functions (e.g.
    submitStagedSession, markStagedSessionReady, etc.) can only be called by
    system_server.
    
    Test: presubmit
    Change-Id: I5877a891636071db6532acb78d54705adff77aeb

--- apexd/apexservice.cpp
+++ apexd/apexservice.cpp
@@ -63,6 +63,16 @@ BinderStatus CheckCallerIsRoot(const std::string& name) {
   return BinderStatus::ok();
 }
 
+BinderStatus CheckCallerSystemOrRoot(const std::string& name) {
+  uid_t uid = IPCThreadState::self()->getCallingUid();
+  if (uid != AID_ROOT && uid != AID_SYSTEM) {
+    std::string msg = "Only root and system_server are allowed to call " + name;
+    return BinderStatus::fromExceptionCode(BinderStatus::EX_SECURITY,
+                                           String8(name.c_str()));
+  }
+  return BinderStatus::ok();
+}
+
 class ApexService : public BnApexService {
  public:
   using BinderStatus = ::android::binder::Status;
@@ -135,6 +145,9 @@ BinderStatus ApexService::stagePackages(const std::vector<std::string>& paths) {
   if (!debug_check.isOk()) {
     return debug_check;
   }
+  if (auto is_root = CheckCallerIsRoot("stagePackages"); !is_root.isOk()) {
+    return is_root;
+  }
   LOG(DEBUG) << "stagePackages() received by ApexService, paths "
              << android::base::Join(paths, ',');
 
@@ -153,6 +166,10 @@ BinderStatus ApexService::stagePackages(const std::vector<std::string>& paths) {
 
 BinderStatus ApexService::unstagePackages(
     const std::vector<std::string>& paths) {
+  if (auto check = CheckCallerSystemOrRoot("unstagePackages"); !check.isOk()) {
+    return check;
+  }
+
   Result<void> res = ::android::apex::UnstagePackages(paths);
   if (res.ok()) {
     return BinderStatus::ok();
@@ -167,6 +184,11 @@ BinderStatus ApexService::unstagePackages(
 
 BinderStatus ApexService::submitStagedSession(const ApexSessionParams& params,
                                               ApexInfoList* apex_info_list) {
+  auto check = CheckCallerSystemOrRoot("submitStagedSession");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "submitStagedSession() received by ApexService, session id "
              << params.sessionId << " child sessions: ["
              << android::base::Join(params.childSessionIds, ',') << "]";
@@ -193,6 +215,11 @@ BinderStatus ApexService::submitStagedSession(const ApexSessionParams& params,
 }
 
 BinderStatus ApexService::markStagedSessionReady(int session_id) {
+  auto check = CheckCallerSystemOrRoot("markStagedSessionReady");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "markStagedSessionReady() received by ApexService, session id "
              << session_id;
   Result<void> success = ::android::apex::MarkStagedSessionReady(session_id);
@@ -207,6 +234,11 @@ BinderStatus ApexService::markStagedSessionReady(int session_id) {
 }
 
 BinderStatus ApexService::markStagedSessionSuccessful(int session_id) {
+  auto check = CheckCallerSystemOrRoot("markStagedSessionSuccessful");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG)
       << "markStagedSessionSuccessful() received by ApexService, session id "
       << session_id;
@@ -222,6 +254,11 @@ BinderStatus ApexService::markStagedSessionSuccessful(int session_id) {
 }
 
 BinderStatus ApexService::markBootCompleted() {
+  auto check = CheckCallerSystemOrRoot("markBootCompleted");
+  if (!check.isOk()) {
+    return check;
+  }
+
   ::android::apex::OnBootCompleted();
   return BinderStatus::ok();
 }
@@ -343,6 +380,11 @@ static std::string ToString(const ApexInfo& package) {
 
 BinderStatus ApexService::getSessions(
     std::vector<ApexSessionInfo>* aidl_return) {
+  auto check = CheckCallerSystemOrRoot("getSessions");
+  if (!check.isOk()) {
+    return check;
+  }
+
   auto sessions = ApexSession::GetSessions();
   for (const auto& session : sessions) {
     ApexSessionInfo session_info;
@@ -355,6 +397,11 @@ BinderStatus ApexService::getSessions(
 
 BinderStatus ApexService::getStagedSessionInfo(
     int session_id, ApexSessionInfo* apex_session_info) {
+  auto check = CheckCallerSystemOrRoot("getStagedSessionInfo");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "getStagedSessionInfo() received by ApexService, session id "
              << session_id;
   auto session = ApexSession::GetSession(session_id);
@@ -372,6 +419,11 @@ BinderStatus ApexService::getStagedSessionInfo(
 
 BinderStatus ApexService::getStagedApexInfos(
     const ApexSessionParams& params, std::vector<ApexInfo>* aidl_return) {
+  auto check = CheckCallerSystemOrRoot("getStagedApexInfos");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "getStagedApexInfos() received by ApexService, session id "
              << params.sessionId << " child sessions: ["
              << android::base::Join(params.childSessionIds, ',') << "]";
@@ -395,6 +447,11 @@ BinderStatus ApexService::getStagedApexInfos(
 
 BinderStatus ApexService::getActivePackages(
     std::vector<ApexInfo>* aidl_return) {
+  auto check = CheckCallerSystemOrRoot("getActivePackages");
+  if (!check.isOk()) {
+    return check;
+  }
+
   auto packages = ::android::apex::GetActivePackages();
   for (const auto& package : packages) {
     ApexInfo apex_info = GetApexInfo(package);
@@ -407,6 +464,11 @@ BinderStatus ApexService::getActivePackages(
 
 BinderStatus ApexService::getActivePackage(const std::string& package_name,
                                            ApexInfo* aidl_return) {
+  auto check = CheckCallerSystemOrRoot("getActivePackage");
+  if (!check.isOk()) {
+    return check;
+  }
+
   Result<ApexFile> apex = ::android::apex::GetActivePackage(package_name);
   if (apex.ok()) {
     *aidl_return = GetApexInfo(*apex);
@@ -416,6 +478,11 @@ BinderStatus ApexService::getActivePackage(const std::string& package_name,
 }
 
 BinderStatus ApexService::getAllPackages(std::vector<ApexInfo>* aidl_return) {
+  auto check = CheckCallerSystemOrRoot("getAllPackages");
+  if (!check.isOk()) {
+    return check;
+  }
+
   const auto& active = ::android::apex::GetActivePackages();
   const auto& factory = ::android::apex::GetFactoryPackages();
   for (const ApexFile& pkg : active) {
@@ -436,6 +503,11 @@ BinderStatus ApexService::getAllPackages(std::vector<ApexInfo>* aidl_return) {
 
 BinderStatus ApexService::installAndActivatePackage(
     const std::string& package_path, ApexInfo* aidl_return) {
+  auto check = CheckCallerSystemOrRoot("installAndActivatePackage");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "installAndActivatePackage() received by ApexService, path: "
              << package_path;
   auto res = InstallPackage(package_path);
@@ -458,6 +530,11 @@ BinderStatus ApexService::preinstallPackages(
     return debug_check;
   }
 
+  auto root_check = CheckCallerIsRoot("preinstallPackages");
+  if (!root_check.isOk()) {
+    return root_check;
+  }
+
   Result<void> res = ::android::apex::PreinstallPackages(paths);
   if (res.ok()) {
     return BinderStatus::ok();
@@ -471,6 +548,11 @@ BinderStatus ApexService::preinstallPackages(
 }
 
 BinderStatus ApexService::abortStagedSession(int session_id) {
+  auto check = CheckCallerSystemOrRoot("abortStagedSession");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "abortStagedSession() received by ApexService.";
   Result<void> res = ::android::apex::AbortStagedSession(session_id);
   if (!res.ok()) {
@@ -482,6 +564,11 @@ BinderStatus ApexService::abortStagedSession(int session_id) {
 }
 
 BinderStatus ApexService::revertActiveSessions() {
+  auto check = CheckCallerSystemOrRoot("revertActiveSessions");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "revertActiveSessions() received by ApexService.";
   Result<void> res = ::android::apex::RevertActiveSessions("", "");
   if (!res.ok()) {
@@ -498,6 +585,11 @@ BinderStatus ApexService::resumeRevertIfNeeded() {
     return debug_check;
   }
 
+  auto root_check = CheckCallerIsRoot("resumeRevertIfNeeded");
+  if (!root_check.isOk()) {
+    return root_check;
+  }
+
   LOG(DEBUG) << "resumeRevertIfNeeded() received by ApexService.";
   Result<void> res = ::android::apex::ResumeRevertIfNeeded();
   if (!res.ok()) {
@@ -510,6 +602,11 @@ BinderStatus ApexService::resumeRevertIfNeeded() {
 
 BinderStatus ApexService::snapshotCeData(int user_id, int rollback_id,
                                          const std::string& apex_name) {
+  auto check = CheckCallerSystemOrRoot("snapshotCeData");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "snapshotCeData() received by ApexService.";
   Result<void> res =
       ::android::apex::SnapshotCeData(user_id, rollback_id, apex_name);
@@ -523,6 +620,11 @@ BinderStatus ApexService::snapshotCeData(int user_id, int rollback_id,
 
 BinderStatus ApexService::restoreCeData(int user_id, int rollback_id,
                                         const std::string& apex_name) {
+  auto check = CheckCallerSystemOrRoot("restoreCeData");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "restoreCeData() received by ApexService.";
   Result<void> res =
       ::android::apex::RestoreCeData(user_id, rollback_id, apex_name);
@@ -535,6 +637,11 @@ BinderStatus ApexService::restoreCeData(int user_id, int rollback_id,
 }
 
 BinderStatus ApexService::destroyDeSnapshots(int rollback_id) {
+  auto check = CheckCallerSystemOrRoot("destroyDeSnapshots");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "destroyDeSnapshots() received by ApexService.";
   Result<void> res = ::android::apex::DestroyDeSnapshots(rollback_id);
   if (!res.ok()) {
@@ -546,6 +653,11 @@ BinderStatus ApexService::destroyDeSnapshots(int rollback_id) {
 }
 
 BinderStatus ApexService::destroyCeSnapshots(int user_id, int rollback_id) {
+  auto check = CheckCallerSystemOrRoot("destroyCeSnapshots");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "destroyCeSnapshots() received by ApexService.";
   Result<void> res = ::android::apex::DestroyCeSnapshots(user_id, rollback_id);
   if (!res.ok()) {
@@ -558,6 +670,11 @@ BinderStatus ApexService::destroyCeSnapshots(int user_id, int rollback_id) {
 
 BinderStatus ApexService::destroyCeSnapshotsNotSpecified(
     int user_id, const std::vector<int>& retain_rollback_ids) {
+  auto check = CheckCallerSystemOrRoot("destroyCeSnapshotsNotSpecified");
+  if (!check.isOk()) {
+    return check;
+  }
+
   LOG(DEBUG) << "destroyCeSnapshotsNotSpecified() received by ApexService.";
   Result<void> res = ::android::apex::DestroyCeSnapshotsNotSpecified(
       user_id, retain_rollback_ids);
